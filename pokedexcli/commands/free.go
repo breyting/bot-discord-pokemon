f65878414cf1441c277249cac082aa55332c7ca3
breyting <gathoyematias@gmail.com>
2025-05-07 18:14:35 +0200

    made a 'no input' exception

--- pokedexcli/commands/free.go
+++ pokedexcli/commands/free.go
@@ -1,6 +1,10 @@
 package commands
 
 func CommandFree(config *Config, data map[string]UserData, input ...string) (string, error) {
+	if len(input) == 0 {
+		return "You need to specify the name of a pokemon to release!\n", nil
+	}
+
 	for key, _ := range data {
 		if input[0] == key {
 			delete(data, key)
